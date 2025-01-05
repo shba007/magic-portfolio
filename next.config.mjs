7d57d64fbbcf95b374b4655c28f520ec4de19af3
Shirsendu Bairagi <shirsendu2001@gmail.com>
2025-01-05 12:37:03 +0530

    feat: update environment variables and configure base URL for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const withPWA = withPWAInit({
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
   output: 'export',
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  basePath: `${process.env.APP_BASE_URL}`,
+  assetPrefix: `${process.env.APP_BASE_URL}/`,
 })
 
 export default nextConfig
